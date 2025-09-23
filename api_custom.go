9e424ecb5af5add1967ad4f70c3145c52b5035d0
keshavamurthi <keshavamurthi@graphiant.com>
2025-09-23 23:35:42 +0000

    TE-3040 : Adding PollAndPutDeviceConfig wrapper to check the device status and push the config

---
+++ api_custom.go
@@ -0,0 +1,67 @@
+package graphiant_sdk
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func GetDeviceStatus(apiClient *APIClient, token string, deviceId int64) bool {
+
+	resp, _, _ := apiClient.DefaultAPI.
+		V1EdgesSummaryGet(context.Background()).
+		Authorization(token).
+		Execute()
+
+	for _, edge := range resp.GetEdgesSummary() {
+		if edge.GetDeviceId() == deviceId {
+			if (edge.GetStatus() == "active" || edge.GetStatus() == "maintenance" || edge.GetStatus() == "staging") && edge.GetPortalStatus() == "Ready" && edge.GetTtConnCount() == 2 {
+				fmt.Printf("Device %s (%d) is ready : Status is %s and Portal Status is Ready\n", edge.GetHostname(), deviceId, edge.GetStatus())
+				return true
+			} else {
+				fmt.Printf("Device %s (%d) is not ready : Status is %s and Portal Status is %s\n", edge.GetHostname(), deviceId, edge.GetStatus(), edge.GetPortalStatus())
+			}
+		}
+	}
+	return false
+}
+
+func PutDeviceConfig(apiClient *APIClient, token string, deviceId int64, deviceConfig V1DevicesDeviceIdConfigPutRequest) *http.Response {
+
+	_, httpRes, err := apiClient.DefaultAPI.
+		V1DevicesDeviceIdConfigPut(context.Background(), deviceId).
+		Authorization(token).
+		V1DevicesDeviceIdConfigPutRequest(deviceConfig).Execute()
+
+	if err != nil {
+		fmt.Printf("Error executing config put request: %v\n", err)
+		return nil
+	}
+	body, _ := io.ReadAll(httpRes.Body)
+	fmt.Printf("Config put response: %s, http status: %v\n", string(body), httpRes.StatusCode)
+	return httpRes
+
+}
+
+func PollAndPutDeviceConfig(apiClient *APIClient, token string, deviceId int64, deviceConfig V1DevicesDeviceIdConfigPutRequest) *http.Response {
+
+	// Poll device status every 30 seconds for 10 attempts
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Polling attempt %d/5 for device %d...\n", i+1, deviceId)
+
+		if GetDeviceStatus(apiClient, token, deviceId) {
+			fmt.Printf("Executing config put request...\n")
+			return PutDeviceConfig(apiClient, token, deviceId, deviceConfig)
+		}
+
+		if i < 9 { // Don't sleep after the last attempt
+			fmt.Printf("Device not ready, waiting 30 seconds...\n")
+			time.Sleep(30 * time.Second)
+		}
+	}
+
+	fmt.Printf("Device %d did not become ready after 10 attempts\n", deviceId)
+	return nil
+}
---
+++ test/config_test.go
@@ -0,0 +1,255 @@
+package graphiant_sdk
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	graphiant_sdk "github.com/Graphiant-Inc/graphiant-sdk-go"
+)
+
+func GetAuthToken() (*graphiant_sdk.APIClient, string) {
+
+	username := os.Getenv("GRAPHIANT_USERNAME")
+	password := os.Getenv("GRAPHIANT_PASSWORD")
+
+	configuration := graphiant_sdk.NewConfiguration()
+	apiClient := graphiant_sdk.NewAPIClient(configuration)
+	authReq := graphiant_sdk.NewV1AuthLoginPostRequest()
+	authReq.SetUsername(username)
+	authReq.SetPassword(password)
+
+	fmt.Printf("Calling V1AuthLoginPost (url: %s)\n", configuration.Servers[0].URL)
+	_, httpRes, _ := apiClient.DefaultAPI.
+		V1AuthLoginPost(context.Background()).
+		V1AuthLoginPostRequest(*authReq).
+		Execute()
+	defer httpRes.Body.Close()
+
+	var result struct {
+		Auth        bool   `json:"auth"`
+		AccountType string `json:"accountType"`
+		Token       string `json:"token"`
+	}
+
+	if err := json.NewDecoder(httpRes.Body).Decode(&result); err != nil {
+		fmt.Printf("Failed to decode auth response: %v\n", err)
+	}
+	fmt.Printf("Auth token: %v\n", result.Token)
+	return apiClient, "Bearer " + result.Token
+}
+
+func UpdateDeviceStatus(deviceId int64, deviceStatus string) {
+	apiClient, token := GetAuthToken()
+
+	bringupReq := graphiant_sdk.NewV1DevicesBringupPutRequest()
+	bringupReq.SetDeviceIds([]int64{deviceId})
+	bringupReq.SetStatus(deviceStatus)
+	fmt.Printf("Calling V1DevicesBringupPutRequest : %v %v",
+		bringupReq.GetDeviceIds(), bringupReq.GetStatus())
+
+	apiClient.DefaultAPI.
+		V1DevicesBringupPut(context.Background()).
+		Authorization(token).
+		V1DevicesBringupPutRequest(*bringupReq).Execute()
+
+}
+
+func ConfigureWan(deviceId int64, interfaceName string, wanCircuit string) {
+	apiClient, token := GetAuthToken()
+
+	circuit := map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeCircuitsValue{
+		wanCircuit: {
+			Name:              graphiant_sdk.PtrString(wanCircuit),
+			ConnectionType:    graphiant_sdk.PtrString("internet_dia"),
+			Label:             graphiant_sdk.PtrString("internet_dia_4"),
+			DiaEnabled:        graphiant_sdk.PtrBool(true),
+			QosProfile:        graphiant_sdk.PtrString("gold25"),
+			LinkDownSpeedMbps: graphiant_sdk.PtrInt32(100),
+			LinkUpSpeedMbps:   graphiant_sdk.PtrInt32(50),
+		},
+	}
+	interfaces := map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValue{
+		interfaceName: {
+			Interface: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterface{
+				AdminStatus: graphiant_sdk.PtrBool(true),
+				Circuit:     graphiant_sdk.PtrString(wanCircuit),
+				Ipv4: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGw{
+					Dhcp: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGwDhcp{
+						DhcpClient: graphiant_sdk.PtrBool(true)},
+				},
+				Ipv6: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGw{
+					Dhcp: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGwDhcp{
+						DhcpClient: graphiant_sdk.PtrBool(true)},
+				},
+			},
+		},
+	}
+	configReq := graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequest()
+	configReq.Edge = graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequestEdge()
+	configReq.Edge.SetInterfaces(interfaces)
+	configReq.Edge.SetCircuits(circuit)
+	circuitsJSON, _ := json.MarshalIndent(configReq.Edge.GetCircuits(), "", "  ")
+	interfacesJSON, _ := json.MarshalIndent(configReq.Edge.GetInterfaces(), "", "  ")
+	fmt.Printf("Calling V1DevicesDeviceIdConfigPut : %v %s %s", deviceId, circuitsJSON, interfacesJSON)
+	graphiant_sdk.PollAndPutDeviceConfig(apiClient, token, deviceId, *configReq)
+}
+
+func ConfigureLan(deviceId int64, interfaceName string, lan string, ipv4Address string, ipv6Address string) {
+	apiClient, token := GetAuthToken()
+
+	interfaces := map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValue{
+		interfaceName: {
+			Interface: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterface{
+				Lan:         graphiant_sdk.PtrString(lan),
+				AdminStatus: graphiant_sdk.PtrBool(true),
+				Ipv4: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGw{
+					Address: &graphiant_sdk.V1GlobalConfigPatchRequestSnmpsValueConfigEngineEndpointsValueEngineEndpointAddressesValue{
+						Address: graphiant_sdk.PtrString(ipv4Address),
+					},
+				},
+				Ipv6: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGw{
+					Address: &graphiant_sdk.V1GlobalConfigPatchRequestSnmpsValueConfigEngineEndpointsValueEngineEndpointAddressesValue{
+						Address: graphiant_sdk.PtrString(ipv6Address),
+					},
+				},
+			},
+		},
+	}
+	configReq := graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequest()
+	configReq.Edge = graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequestEdge()
+	configReq.Edge.SetInterfaces(interfaces)
+	interfacesJSON, _ := json.MarshalIndent(configReq.Edge.GetInterfaces(), "", "  ")
+
+	fmt.Printf("Calling V1DevicesDeviceIdConfigPut : %v %s\n", deviceId, interfacesJSON)
+	graphiant_sdk.PollAndPutDeviceConfig(apiClient, token, deviceId, *configReq)
+}
+
+func ConfigureDefaultLan(deviceId int64, interfaceName string, defaultLan string) {
+	apiClient, token := GetAuthToken()
+
+	interfaces := map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValue{
+		interfaceName: {
+			Interface: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterface{
+				Lan: graphiant_sdk.PtrString(defaultLan),
+			},
+		},
+	}
+	configReq := graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequest()
+	configReq.Edge = graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequestEdge()
+	configReq.Edge.SetInterfaces(interfaces)
+	interfacesJSON, _ := json.MarshalIndent(configReq.Edge.GetInterfaces(), "", "  ")
+
+	fmt.Printf("Calling V1DevicesDeviceIdConfigPut : %v %s\n", deviceId, interfacesJSON)
+	graphiant_sdk.PollAndPutDeviceConfig(apiClient, token, deviceId, *configReq)
+}
+
+func ConfigureSubInterface(deviceId int64, interfaceName string, lan string, vlan int32, ipv4Address string, ipv6Address string) {
+	apiClient, token := GetAuthToken()
+
+	interfaces := map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValue{
+		interfaceName: {
+			Interface: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterface{
+				Subinterfaces: &map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterfaceSubinterfacesValue{
+					fmt.Sprintf("%d", vlan): {
+						Interface: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterfaceSubinterfacesValueInterface{
+							AdminStatus: graphiant_sdk.PtrBool(true),
+							Ipv4: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGw{
+								Address: &graphiant_sdk.V1GlobalConfigPatchRequestSnmpsValueConfigEngineEndpointsValueEngineEndpointAddressesValue{
+									Address: graphiant_sdk.PtrString(ipv4Address),
+								},
+							},
+							Ipv6: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestCoreInterfacesValueInterfaceGwGw{
+								Address: &graphiant_sdk.V1GlobalConfigPatchRequestSnmpsValueConfigEngineEndpointsValueEngineEndpointAddressesValue{
+									Address: graphiant_sdk.PtrString(ipv6Address),
+								},
+							},
+							Lan:  graphiant_sdk.PtrString(lan),
+							Vlan: graphiant_sdk.PtrInt32(vlan),
+						},
+					},
+				},
+			},
+		},
+	}
+	configReq := graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequest()
+	configReq.Edge = graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequestEdge()
+	configReq.Edge.SetInterfaces(interfaces)
+	interfacesJSON, _ := json.MarshalIndent(configReq.Edge.GetInterfaces(), "", "  ")
+
+	fmt.Printf("Calling V1DevicesDeviceIdConfigPut : %v %s\n", deviceId, interfacesJSON)
+	graphiant_sdk.PollAndPutDeviceConfig(apiClient, token, deviceId, *configReq)
+}
+
+func DeleteSubInterface(deviceId int64, interfaceName string, vlan int32) {
+	apiClient, token := GetAuthToken()
+
+	interfaces := map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValue{
+		interfaceName: {
+			Interface: &graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterface{
+				Subinterfaces: &map[string]graphiant_sdk.V1DevicesDeviceIdConfigPutRequestEdgeInterfacesValueInterfaceSubinterfacesValue{
+					fmt.Sprintf("%d", vlan): {},
+				},
+			},
+		},
+	}
+	configReq := graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequest()
+	configReq.Edge = graphiant_sdk.NewV1DevicesDeviceIdConfigPutRequestEdge()
+	configReq.Edge.SetInterfaces(interfaces)
+	interfacesJSON, _ := json.MarshalIndent(configReq.Edge.GetInterfaces(), "", "  ")
+
+	fmt.Printf("Calling V1DevicesDeviceIdConfigPut : %v %s\n", deviceId, interfacesJSON)
+	graphiant_sdk.PollAndPutDeviceConfig(apiClient, token, deviceId, *configReq)
+}
+
+func Test_configure_wan_interface(t *testing.T) {
+	// Note: This test requires GRAPHIANT_USERNAME and GRAPHIANT_PASSWORD environment variables to be set
+	// export GRAPHIANT_USERNAME="your_username"
+	// export GRAPHIANT_PASSWORD="your_password"
+
+	t.Skip("skip test") // remove to run test
+
+	ConfigureWan(30000048295, "GigabitEthernet7/0/0", "c-gigabitethernet7-0-0")
+}
+
+func Test_configure_lan_interface(t *testing.T) {
+	// Note: This test requires GRAPHIANT_USERNAME and GRAPHIANT_PASSWORD environment variables to be set
+	// export GRAPHIANT_USERNAME="your_username"
+	// export GRAPHIANT_PASSWORD="your_password"
+
+	t.Skip("skip test") // remove to run test
+
+	ConfigureLan(30000048295, "GigabitEthernet7/0/0", "lan-7-test", "10.2.7.2/24", "2001:10:2:7::2/64")
+}
+
+func Test_deconfigure_lan_interface(t *testing.T) {
+	// Note: This test requires GRAPHIANT_USERNAME and GRAPHIANT_PASSWORD environment variables to be set
+	// export GRAPHIANT_USERNAME="your_username"
+	// export GRAPHIANT_PASSWORD="your_password"
+
+	t.Skip("skip test") // remove to run test
+
+	ConfigureDefaultLan(30000048295, "GigabitEthernet7/0/0", "default-10000000335")
+}
+
+func Test_configure_sub_interface(t *testing.T) {
+	// Note: This test requires GRAPHIANT_USERNAME and GRAPHIANT_PASSWORD environment variables to be set
+	// export GRAPHIANT_USERNAME="your_username"
+	// export GRAPHIANT_PASSWORD="your_password"
+
+	t.Skip("skip test") // remove to run test
+
+	ConfigureSubInterface(30000048295, "GigabitEthernet7/0/0", "lan-7-test", 28, "10.2.7.2/24", "2001:10:2:7::2/64")
+}
+
+func Test_delete_sub_interface(t *testing.T) {
+	// Note: This test requires GRAPHIANT_USERNAME and GRAPHIANT_PASSWORD environment variables to be set
+	// export GRAPHIANT_USERNAME="your_username"
+	// export GRAPHIANT_PASSWORD="your_password"
+
+	t.Skip("skip test") // remove to run test
+
+	DeleteSubInterface(30000048295, "GigabitEthernet7/0/0", 28)
+}
